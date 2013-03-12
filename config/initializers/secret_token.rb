10619202727e0437dcdbae1437188e658f4846a2
robinvdijk <robin_van_dijk@hotmail.nl>
2013-03-12 14:28:00 +0100

    first commit again

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Greendrive::Application.config.secret_token = '1bae055ab00e21dccf51477e611b50688c7ef8fa8840713be4b2bf901785d5ce866178846df536f71cc76c4e1b4d69c8bdb307f56fc8095055fe21aa7b4a7bf4'
