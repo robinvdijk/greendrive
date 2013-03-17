5760e003767f0972d496adacd65627485c86e6d5
Dennis91 <dennis_wilpshaar13@hotmail.com>
2013-03-17 15:11:41 +0100

    addavatar

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,11 +1,14 @@
 class User < ActiveRecord::Base
-  attr_accessible :email, :first_name, :last_name, :license_plate, :password_confirmation, :password, :user_name
+  attr_accessible :email, :first_name, :last_name, :license_plate, :password_confirmation, :password, :user_name, :avatar
   has_secure_password
   validates_presence_of :password, :on => :create
 
   validates :first_name, :presence => true
   validates :last_name, :presence => true
   validates :user_name, :presence => true, :uniqueness => true, :length => {:maximum => 16}
-  validates :license_plate, :presence => true, :uniqueness => true, :length {:minimum => 6, :maximum => 6}
+  validates :license_plate, :presence => true, :uniqueness => true, :length => {:minimum => 6, :maximum => 6}
+  
+  #used to add avatar to user profile
+  mount_uploader :avatar, AvatarUploader
 
 end
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20130314114439) do
+ActiveRecord::Schema.define(:version => 20130315135520) do
 
   create_table "users", :force => true do |t|
     t.string   "email"
@@ -22,6 +22,7 @@ ActiveRecord::Schema.define(:version => 20130314114439) do
     t.string   "license_plate"
     t.datetime "created_at",      :null => false
     t.datetime "updated_at",      :null => false
+    t.string   "avatar"
   end
 
 end
