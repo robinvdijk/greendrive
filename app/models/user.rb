70bdd860b8d2b39e20c88380d7be22e82c068534
robinvdijk <robin_van_dijk@hotmail.nl>
2013-03-18 09:49:23 +0100

    Veel shit

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,10 +2,11 @@ class User < ActiveRecord::Base
   attr_accessible :email, :first_name, :last_name, :license_plate, :password_confirmation, :password, :user_name, :birthday
   has_secure_password
   validates_presence_of :password, :on => :create
-
+  
   validates :first_name, :presence => true
   validates :last_name, :presence => true
   validates :user_name, :presence => true, :uniqueness => true, :length => {:maximum => 16}
   validates :license_plate, :presence => true, :uniqueness => true, :length => {:minimum => 6, :maximum => 6}
+
 
 end
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20130315185525) do
+ActiveRecord::Schema.define(:version => 20130318062016) do
 
   create_table "users", :force => true do |t|
     t.string   "email"
@@ -22,7 +22,7 @@ ActiveRecord::Schema.define(:version => 20130315185525) do
     t.string   "license_plate"
     t.datetime "created_at",      :null => false
     t.datetime "updated_at",      :null => false
-    t.datetime "birthday"
+    t.date     "birthday"
   end
 
 end
