b028abbe5640093ee952240f93a370586af84acc
robinvdijk <robin_van_dijk@hotmail.nl>
2013-03-18 09:57:44 +0100

    Goeie

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,8 +1,8 @@
 class User < ActiveRecord::Base
-  attr_accessible :email, :first_name, :last_name, :license_plate, :password_confirmation, :password, :user_name, :birthday
+  attr_accessible :email, :first_name, :last_name, :license_plate, :clean_data, :password_confirmation, :password, :user_name, :birthday
   has_secure_password
   validates_presence_of :password, :on => :create
-  
+
   validates :first_name, :presence => true
   validates :last_name, :presence => true
   validates :user_name, :presence => true, :uniqueness => true, :length => {:maximum => 16}
