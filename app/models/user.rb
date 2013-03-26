ac130d6bc18f7c24cf05706f9464806df6636b94
Dennis91 <dennis_wilpshaar13@hotmail.com>
2013-03-26 11:10:28 +0100

    UpdateCSS

--- app/models/user.rb
+++ app/models/user.rb
@@ -15,15 +15,14 @@ class User < ActiveRecord::Base
   validates :user_name, :presence => true, :uniqueness => true, :length => {:minimum => 4,:maximum => 20}
   validates :license_plate, :presence => true, :uniqueness => true, :length => {:minimum => 6, :maximum => 6}
   validates :email, :uniqueness => true, :format => { :with => /\A([^@\s]+)@((?:[-a-z0-9]+\.)+[a-z]{2,})\Z/i}
-<<<<<<< HEAD
+
   # Dit is voor het registeren form
   validates :terms_of_service, :acceptance => true
   validates :terms_of_privacy, :acceptance => true
 
 
 
-=======
->>>>>>> 6583f190510596418c0437a461f3d0c6ea8e88d2
+
   before_create { generate_token(:auth_token) }
   
   before_validation :clean_data #Filters out whitespaces and special characters
