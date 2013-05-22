e2e7730752ce5237c112a037f8d9eee8109fed6e
Dennis91 <dennis_wilpshaar13@hotmail.com>
2013-05-22 14:19:33 +0200

    dennis22/05

---
+++ app/assets/javascripts/homepage.js
@@ -0,0 +1,9 @@
+$(function(){
+	$('.col_home_side div').each(function(idx){
+		$(this).hover(function(evt){
+			var link = $(evt.target);
+			var selector = '#' + link.data('explain_block');
+			$(selector).toggle();
+		})
+	})
+})
