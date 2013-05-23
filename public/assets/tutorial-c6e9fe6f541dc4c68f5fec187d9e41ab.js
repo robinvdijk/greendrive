d4530380972dad8ff277355167c1bcede413e9da
Dennis91 <dennis_wilpshaar13@hotmail.com>
2013-05-23 17:53:31 +0200

    deploynew

---
+++ public/assets/tutorial-c6e9fe6f541dc4c68f5fec187d9e41ab.js
@@ -0,0 +1,1 @@
+function tutorial(){$("#tutorial_1, #blanket").toggle()}function profile(){$("#tutorial_2, #tutorial_1").toggle(),$("#profile").css({position:"relative"})}function badges(){$("#tutorial_2").toggle(),$("#profile, #badges").css({position:"relative"})}
