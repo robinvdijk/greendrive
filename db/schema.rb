37118c68815808d14c2e31955eecb3abd5c9df52
jeroenstevens <jeroenstevens@live.nl>
2013-06-05 15:42:52 +0200

    logout in menu van dashboard fix

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20130604134827) do
+ActiveRecord::Schema.define(:version => 20130604131952) do
 
   create_table "achievements", :force => true do |t|
     t.integer  "badge_id"
